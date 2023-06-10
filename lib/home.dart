081b59aff90eb782517d079f1ac6b5779ce6e2af
Mustafa Özcan <mustafa452017@gmail.com>
2023-06-10 15:45:17 +0300

    comit

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class WrapWidgetKullanimi extends StatelessWidget {
-  const WrapWidgetKullanimi({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
